854ab9decdb015f52780963276c0bf2f6cf3d690
Julius Koronci <julius.koronci@external.erstegroup.com>
2020-10-12 22:18:58 +0200

    chore(api): avoid dto extension

--- Application/Profile/Queries/GetProfileDto.cs
+++ Application/Profile/Queries/GetProfileDto.cs
@@ -1,6 +1,18 @@
+using System;
+
 namespace Application.Profile.Queries
 {
-    public class GetProfileDto : Domain.Entities.Profile
+    public class GetProfileDto
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string WebSite { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
     }
 }
